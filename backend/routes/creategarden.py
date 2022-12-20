e4480798a34ae3ed2c314f0dfd61d6d752567708
kour0 <lucas.laurent06@gmail.com>
2022-12-20 17:27:54 +0100

    creategarden.py

--- backend/routes/creategarden.py
+++ backend/routes/creategarden.py
@@ -4,7 +4,6 @@ from decouple import config
 from flask import Blueprint, request, make_response
 from flask_cors import CORS
 
-
 from bdd import Session
 from middlewares import auth
 from models.Garden import Garden
@@ -16,18 +15,21 @@ CORS(creategarden, supports_credentials=True)
 
 BASE_URL = '/api/'
 
+
 @creategarden.post(BASE_URL + '/creategarden')
 def create():
     try:
         user = auth.authenticate(request)
         body = request.get_json()
-        gardenName = body['gardenName']
+        garden_name = body['gardenName']
         country = body['country']
-        streetAddress = body['streetAddress']
+        street_address = body['streetAddress']
         city = body['city']
         region = body['region']
-        postalCode = body['postalCode']
-        garden = Garden(owner=user['username'], garden_name=gardenName, manager=user['username'], country=country, city=city, province=region, postal_code=postalCode)
+        postal_code = body['postalCode']
+        garden = Garden(owner=user['username'], garden_name=garden_name, manager=user['username'],
+                        street_address=street_address, country=country, city=city, province=region,
+                        postal_code=postal_code)
         session.add(garden)
         session.commit()
         print(body)
