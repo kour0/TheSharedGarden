e983ee8304435dd66c531931c190ef99e09fd959
kour0 <lucas.laurent06@gmail.com>
2022-12-22 00:41:03 +0100

    join garden with field + carte

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -2,8 +2,11 @@ import Iframe from 'react-iframe'
 
 export function Map() {
   return (
+    // use base tag to make relative links work
     <div>
-      <Iframe url="http://127.0.0.1:5454/map"
+      <Iframe
+        target='_parent'
+        url="http://127.0.0.1:5454/map"
         width="80%"
         height="615px"
         display="initial"
