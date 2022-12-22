bed401187e7aca930e61a23f6da3a3a5842f9c46
kour0 <lucas.laurent06@gmail.com>
2022-12-22 17:46:03 +0100

    correction

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -4,13 +4,9 @@ export function Map() {
   return (
     // use base tag to make relative links work
     <div>
-<<<<<<< HEAD
-      <Iframe url="http://127.0.0.1:5454/api/map"
-=======
       <Iframe
         target='_parent'
         url="http://127.0.0.1:5454/map"
->>>>>>> master
         width="80%"
         height="615px"
         display="initial"
