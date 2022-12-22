39721652be57be09a2e0195b29d77e8eafcceed1
unshade <72690981+unshade@users.noreply.github.com>
2022-12-22 20:53:36 +0100

    profile request example

--- frontend/src/pages/app/profile.jsx
+++ frontend/src/pages/app/profile.jsx
@@ -1,35 +1,30 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import { useQuery } from "@tanstack/react-query"
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 
 import axios from 'axios';
 import { Loader } from "../../components/loader/FullScreenLoader";
+import { request } from "../../utils/axios-utils";
 
 
 export default function Profile() {
-  const { isLoading, isError, data, error } = useQuery(['profile'], () => {
-    try {
-      const response = request({ url: '/api/profile', method: 'get' });
-      return response.data;
-    } catch (error) {
-      console.log('prof');
-    }
-  });
 
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [previewUrl, setPreviewUrl] = useState(null);
-  const handleImageChange = (event) => {
-    const image = event.target.files[0];
-    setSelectedImage(image);
-
-    const { isLoading, isError, data, error } = useQuery(['profile'], () => axios.get(url, { withCredentials: true, }).then(res => res.data));
+    const { isLoading, isError, data, error } = useQuery(['profile'], async () => {
+        try {
+          const response = await request({ url: '/api/profile', method: 'get' });
+          return response.data;
+        } catch (error) {
+          console.log('prof');
+        }
+      });
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm()
+    
     const onSubmit = async (data) => {
         const formData = new FormData();
         //formData.append('file', selectedImage);
@@ -60,52 +55,20 @@ export default function Profile() {
         }
     }
 
-  return (
-    <>
-      {isLoading && <Loader />}
-      {isError && <div>Error: {error.message}</div>}
-      {data && (
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:py-8">
-          <div className="text-center mt-5 mb-10">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mon compte</h2>
-            <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
-            </p>
-          </div>
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
 
-          <div>
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-              <div className="md:col-span-1">
-                <div className="px-4 sm:px-0">
-                  <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-                  <p className="mt-1 text-sm text-gray-600">
-                    Ces informations seront affichées publiquement. Faites donc attention à ce que vous partagez.{' '}
-                  </p>
-                </div>
-              </div>
-              <div className="mt-5 md:col-span-2 md:mt-0">
-                <form action="#" method="POST">
-                  <div className="shadow sm:overflow-hidden sm:rounded-md">
-                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                      <div className="grid grid-cols-3 gap-6">
-                        <div className="col-span-3 sm:col-span-2">
-                          <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                            Pseudo
-                          </label>
-                          <input
-                            type="text"
-                            name="pseudo"
-                            id="pseudo "
-                            autoComplete="given-name"
-                            defaultValue={data.username}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          />
-                        </div>
-                      </div>
+    const handleImageChange = event => {
+        const image = event.target.files[0];
+        setSelectedImage(image);
+
+        const reader = new FileReader();
+        reader.onload = e => setPreviewUrl(e.target.result);
+        reader.readAsDataURL(image);
+    };
 
     return (
         <>
-
             {isLoading && < Loader />}
             {isError && <div>Error: {error.message}</div>}
             {data && (
@@ -197,21 +160,13 @@ export default function Profile() {
                                 </form>
                             </div>
                         </div>
-                      </div>
                     </div>
-                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                      <button
-                        type="submit"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Enregistrer
-                      </button>
+
+                    <div className="hidden sm:block" aria-hidden="true">
+                        <div className="py-5">
+                            <div className="border-t border-gray-200" />
+                        </div>
                     </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
 
                     <div className="mt-10 sm:mt-0">
                         <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -272,77 +227,9 @@ export default function Profile() {
                         </div>
                     </div>
 
-          <div className="mt-10 sm:mt-0">
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-              <div className="md:col-span-1">
-                <div className="px-4 sm:px-0">
-                  <h3 className="text-lg font-medium leading-6 text-gray-900">Informations personnelles</h3>
-                  <p className="mt-1 text-sm text-gray-600">Rien pour le moment</p>
                 </div>
-              </div>
-              <div className="mt-5 md:col-span-2 md:mt-0">
-                <form action="#" method="POST">
-                  <div className="overflow-hidden shadow sm:rounded-md">
-                    <div className="bg-white px-4 py-5 sm:p-6">
-                      <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                          <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                            Prénom
-                          </label>
-                          <input
-                            type="text"
-                            name="first-name"
-                            id="first-name"
-                            autoComplete="given-name"
-                            defaultValue={data.first_name}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          />
-                        </div>
+            )}
+        </>
 
-                        <div className="col-span-6 sm:col-span-3">
-                          <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                            Nom
-                          </label>
-                          <input
-                            type="text"
-                            name="last-name"
-                            id="last-name"
-                            autoComplete="family-name"
-                            defaultValue={data.last_name}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          />
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-4">
-                          <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                            Adresse Email
-                          </label>
-                          <input
-                            type="text"
-                            name="email-address"
-                            id="email-address"
-                            autoComplete="email"
-                            defaultValue={data.email}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                      <button
-                        type="submit"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Enregistrer
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+    )
 }
