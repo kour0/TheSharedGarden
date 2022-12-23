5e8746d510d600eb4115664ac06fb4df6bc6e88f
unshade <72690981+unshade@users.noreply.github.com>
2022-12-23 22:03:15 +0100

    ignored poor files :(

---
+++ frontend/src/lib/app/dashboard.js
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { request } from '../../utils/axios-utils';
+
+export const getGardens = () => {
+  const response = useQuery(['garden'], async () => {
+    try {
+      const response = await request({ url: '/api/garden/', method: 'GET' });
+      return response.data;
+    } catch (error) {
+      console.warn(error);
+    }
+  });
+  return response;
+};
