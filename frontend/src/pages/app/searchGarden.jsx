05f8951120808ab8be94708a197e81c6c1906547
unshade <72690981+unshade@users.noreply.github.com>
2022-12-23 23:03:30 +0100

    searchGarden react

--- frontend/src/pages/app/searchGarden.jsx
+++ frontend/src/pages/app/searchGarden.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import { GardenCard } from '../../components/GardenCard';
+import { Loader } from '../../components/loader/FullScreenLoader';
 import { request } from '../../utils/axios-utils';
 
 export default function SearchGarden() {
@@ -13,21 +15,30 @@ export default function SearchGarden() {
     }
   });
   console.log(data);
-  return !isLoading ? (
-    <div>
-      <h1>Search Garden</h1>
-      <p>
-        {data.map((garden) => {
-          return (
-            <div key={garden.id}>
-              <h2>{garden.name}</h2>
-              <p>{garden.street_address}</p>
+  return (
+    <>
+      {isLoading && <Loader />}
+      {data && (
+        <div className="relative bg-gray-50 px-4 pb-16 sm:px-6 lg:px-8 lg:pb-28">
+          <div className="absolute inset-0">
+            <div className="h-1/3 bg-white sm:h-2/3" />
+          </div>
+          <div className="relative pt-3 lg:pt-8 mx-auto max-w-7xl">
+            <div className="text-center mt-5">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Jardins</h2>
+              <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
+                Liste de jardins qui semblent correspondre à votre recherche.
+              </p>
             </div>
-          );
-        })}
-      </p>
-    </div>
-  ) : (
-    <></>
+            <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
+              {data.map((garden, index) => (
+                <GardenCard key={index} garden={garden}></GardenCard>
+              ))}
+            </div>
+          </div>
+        </div>
+        
+      )}
+    </>
   );
 }
