cf93350565ac001bf68d27a5dfdb9240552f32d1
kour0 <lucas.laurent06@gmail.com>
2022-12-24 17:18:13 +0100

    Map.jsx

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,16 +1,41 @@
 import Iframe from 'react-iframe';
+import { useQuery } from '@tanstack/react-query';
+import { request } from '../utils/axios-utils';
+import { useState } from 'react';
 
 export function Map() {
+
+  const [map, setMap] = useState(null)
+
+  const {
+    isLoading,
+    isError,
+    data,
+    error,
+  } = useQuery(['Map'], async () => {
+    const response = await request({ url: '/api/map', method: 'get', responseType: 'blob' });
+    return response.data;
+  });
+
+  if (!isLoading && !isError) {
+    // response is a html file
+    const reader = new FileReader();
+    // console log the html file
+    reader.onload = (e) => setMap(e.target.result);
+    reader.readAsDataURL(data);
+  }
+
   return (
     // use base tag to make relative links work
     <div>
       <Iframe
         target="_parent"
-        url="http://127.0.0.1:5454/map"
+        url={map}
         width="80%"
         height="615px"
         display="initial"
         position="relative"
+        sandbox={["allow-same-origin","allow-scripts","allow-popups","allow-forms","allow-modals"]}
         allowFullScreen
       />
     </div>
