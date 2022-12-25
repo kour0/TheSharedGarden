6f56e5ef612edc9296d78f846a2a50596a37e7b9
Alexis MARCEL <marcel16u@etu.univ-lorraine.fr>
2022-12-25 19:09:00 +0100

    update ui dashboard

--- frontend/src/pages/app/dashboard.jsx
+++ frontend/src/pages/app/dashboard.jsx
@@ -10,9 +10,14 @@ export default function Dashboard() {
   return !isLoading ? (
     <>
       <div className="relative bg-gray-50 px-4 pb-16 sm:px-6 lg:px-8 lg:pb-28">
-        <div className="absolute inset-0">
-          <div className="h-1/3 bg-white sm:h-2/3" />
-        </div>
+        {data.length !== 0 ? (
+          <div className="absolute inset-0">
+            <div className="h-1/3 bg-white sm:h-2/3" />
+          </div>
+        ) : (
+          ''
+        )}
+
         <div className="relative pt-3 lg:pt-8 mx-auto max-w-7xl">
           <div className="text-center mt-5">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mes jardins</h2>
@@ -21,25 +26,23 @@ export default function Dashboard() {
             </p>
           </div>
           {data.length === 0 ? (
-              <div className="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:py-16 lg:px-8">
-                <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
-                  <span className="block">Vous n'avez pas encore de jardins ?</span>
-                  <span className="block">Créez-en un dès maintenant !</span>
-                </h2>
-                <div className="mt-8 flex justify-center">
-                  <div className="inline-flex rounded-md shadow">
-                    <Link
-                      to="/app/create-garden"
-                      relative='path'
-                      className="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-700 px-5 py-3 text-base font-medium text-white hover:bg-teal-800"
-                    >
-                      Créer un jardin
-                    </Link>
-                  </div>
-                  
+            <div className="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:py-16 lg:px-8">
+              <h2 className="text-l tracking-tight text-gray-500 sm:text-xl">
+                <span className="block">Vous n'avez pas encore de jardins ?</span>
+                <span className="block">Créez-en un dès maintenant !</span>
+              </h2>
+              <div className="mt-8 flex justify-center">
+                <div className="inline-flex rounded-md shadow">
+                  <Link
+                    to="/app/create-garden"
+                    relative="path"
+                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-700 px-5 py-3 text-base font-medium text-white hover:bg-teal-800"
+                  >
+                    Créer un jardin
+                  </Link>
                 </div>
               </div>
-          
+            </div>
           ) : (
             <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
               {data.map((garden, index) => (
