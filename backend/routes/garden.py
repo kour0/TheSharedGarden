5b143ea34d79ab07baf3cdeeb931d1e62fad05dc
Alexis MARCEL <marcel16u@etu.univ-lorraine.fr>
2022-12-25 19:03:10 +0100

    --

--- backend/routes/garden.py
+++ backend/routes/garden.py
@@ -12,7 +12,6 @@ from models.Accounts import Accounts
 from models.Garden import Garden
 from models.Link import Link
 from routes.map import add_map
-import time
 
 garden = Blueprint('garden', __name__)
 session = Session()
@@ -96,7 +95,6 @@ def get_gardens(garden_name):
 @garden.post(BASE_URL + '/create')
 def create():
     try:
-        time.sleep(10)
         user = g.user
         # Recupération de l'image
         image = request.files['file']
