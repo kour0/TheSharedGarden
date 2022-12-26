abd9c488d8bad4032ea809e4f0cfb4a029683ec2
unshade <72690981+unshade@users.noreply.github.com>
2022-12-26 22:42:01 +0100

    garden grid

--- frontend/src/pages/app/garden.jsx
+++ frontend/src/pages/app/garden.jsx
@@ -1,11 +1,44 @@
-import { useParams } from "react-router-dom";
-import { getGarden } from "../../lib/gardens";
+import { useParams } from 'react-router-dom';
+import { getGarden } from '../../lib/gardens';
+import { useDroppable } from '@dnd-kit/core';
+import { Loader } from '../../components/loader/FullScreenLoader';
+import { useMemo, useState } from 'react';
 
 export default function Garden() {
   const { gardenId } = useParams();
-  const { data, isLoading, isError } = getGarden(gardenId);
+  const { isLoading, isError, data, error } = getGarden(gardenId);
+  const { setNodeRef, isOver } = useDroppable({
+    id: 'garden',
+  });
+
+  const grid = useMemo(() => {
+    const grid = [];
+    for (let i = 0; i < 12; i++) {
+      for (let j = 0; j < 12; j++) {
+        grid.push({ x: i, y: j });
+      }
+    }
+    return grid;
+  }
+  , []);
+
   console.log(data);
+
   return !isLoading ? (
-    <div>Gestion du jardin {gardenId} aka {data.name}</div>
-  ) : <></>
+    <div className='grid grid-cols-12 max-w-md gap-2 mx-auto w-full mt-24'>
+      {
+        grid.map((cell, index) => (
+          <div
+            key={index}
+            className={`h-8 rounded bg-gray-200 border border-gray-300${
+              isOver ? 'bg-green-200' : ''
+            }`}
+          ></div>
+        ))
+      }
+    </div>
+  ) : (
+    <Loader />
+  );
+
 }
