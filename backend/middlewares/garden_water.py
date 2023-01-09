3a081964ac34e8e0e8f36731fcae98083515b250
Lucas Laurent <114945778+kour0@users.noreply.github.com>
2023-01-09 22:16:34 +0100

    algorithme opti source eau

--- backend/middlewares/garden_water.py
+++ backend/middlewares/garden_water.py
@@ -1,119 +1,50 @@
-from copy import deepcopy
-from time import time
-
+from gurobipy import *
 import numpy as np
+import gurobipy as gp
 
 
-def trouver_points_d_eau(potager, n, m, potager_voulue, parcelle_a_arroser):
-    def est_valide(i, j, potager):
-        # Vérifie si la case (i, j) est valide pour y placer un point d'eau
-        if 0 <= i < n and 0 <= j < m and potager[i][j] >= potager_voulue[i][j] > 1:
-            return False
-        if 0 <= i < n and 0 <= j < m and potager_voulue[i][j] == 1:
-            test = False
-            for k in range(i - 1, i + 2):
-                for l in range(j - 1, j + 2):
-                    test = test or (0 <= k < n and 0 <= l < m and potager_voulue[k][l] > 1)
-            return test
-        return 0 <= i < n and 0 <= j < m and potager_voulue[i][j] > 0
+def solveur_sources(M):
+    # Create a new model
+    m = gp.Model("sources")
+    longueur = len(M)
+    # Create variables
+    X = m.addMVar(longueur * longueur, vtype=GRB.BINARY, name="X")
 
-    def backtrack(potager_clean, i, j, points_d_eau_clean, taille_optimale, parcelle_arrosee):
-        global potager_voulue
-        # Vérifie si on a parcouru toutes les cases du potager
-        if taille_optimale <= len(points_d_eau_clean):
-            return float('inf'), float('inf')
-        if i == n and j == m:
-            # Si oui, vérifie si toutes les plantes ont été arrosées par rapport à la potager_voulue
-            if parcelle_arrosee != parcelle_a_arroser:
-                return float('inf'), float('inf')
-            taille_optimale = len(points_d_eau_clean)
-            # Si toutes les plantes ont été arrosées, on retourne le nombre de points d'eau utilisés
-            return points_d_eau_clean, taille_optimale
+    # Construction de la matrice des contraintes C
+    C = []
+    for i in range(longueur):
+        for j in range(longueur):
+            if M[i, j] >= 1:
+                add = [0 for m in range(longueur * longueur)]
+                for k in range(-1, 2):
+                    for l in range(-1, 2):
+                        if 0 <= i + k < longueur and 0 <= j + l < longueur:
+                            add[(i + k) * longueur + j + l] = 1
+                # On ajoute add au tableau numpy C
+                C.append(add)
+    C = np.array(C)
+    # Construction de la matrice B
+    B = []
+    for i in range(longueur):
+        for j in range(longueur):
+            if M[i, j] >= 1:
+                B.append(M[i, j])
+    B = np.array(B)
+    # On ajoute la contrainte
+    m.addConstr(C @ X >= B, name="contrainte_des_légumes")
 
-        # Si on a parcouru toutes les colonnes de la ligne i, on passe à la ligne suivante
-        if j == m:
-            i += 1
-            j = 0
+    # Set objective
+    m.setObjective(X.sum(), GRB.MINIMIZE)
 
-        solution = float('inf')
+    # Optimize model
+    m.optimize()
 
-        # Si la case (i, j) est valide pour y placer un point d'eau, on essaie cette solution
-        if est_valide(i, j, potager_clean):
-            points_d_eau = deepcopy(points_d_eau_clean)
-            points_d_eau.append((i, j))
-            # On marque la case (i, j) et les cases autour comme arrosées
-            for x in range(i - 1, i + 2):
-                for y in range(j - 1, j + 2):
-                    if (x == i and y == j) or (0 <= x < n and 0 <= y < m and potager_voulue[i][j] > 1):
-                        potager[x][y] += 1
-                        if potager[x][y] == potager_voulue[x][y]:
-                            parcelle_arrosee += 1
-            solution_avec_point_d_eau, taille_optimale_potentielle = backtrack(potager, i, j + 1, points_d_eau
-                                                                               , taille_optimale, parcelle_arrosee)
-            if taille_optimale_potentielle < taille_optimale:
-                taille_optimale = taille_optimale_potentielle
-                solution = solution_avec_point_d_eau
-            # On démarque la case (i, j) et les cases autour
-            for x in range(i - 1, i + 2):
-                for y in range(j - 1, j + 2):
-                    if (x == i and y == j) or (0 <= x < n and 0 <= y < m and potager_voulue[i][j] > 1):
-                        potager[x][y] -= 1
-                        if potager[x][y] + 1 == potager_voulue[x][y]:
-                            parcelle_arrosee -= 1
-            # On essaie de ne pas placer de point d'eau à la case (i, j)
-            solution_sans_point_d_eau, taille_optimale_potentielle = backtrack(potager, i, j + 1, points_d_eau_clean,
-                                                                               taille_optimale, parcelle_arrosee)
-            if taille_optimale_potentielle < taille_optimale:
-                taille_optimale = taille_optimale_potentielle
-                solution = solution_sans_point_d_eau
-        # Si la case (i, j) n'est pas valide pour y placer un point d'eau, on essaie de ne pas placer de point d'eau
-        # à la case (i, j)
-        else:
-            # On essaie la solution sans point d'eau
-            solution_sans_point_d_eau, taille_optimale_potentielle = backtrack(potager_clean, i, j + 1,
-                                                                               points_d_eau_clean, taille_optimale,
-                                                                               parcelle_arrosee)
-            if taille_optimale_potentielle != float('inf') and taille_optimale_potentielle < taille_optimale:
-                taille_optimale = taille_optimale_potentielle
-                solution = solution_sans_point_d_eau
-        return solution, taille_optimale
+    # Print solution
+    if m.status == GRB.Status.OPTIMAL:
+        X_opt = np.array(m.X)
 
-    return backtrack(potager, 0, 0, [], 6, 0)
-
-
-# Exemple de potager de dimensions 7x7
-potager = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1]
-]
-
-potager = np.array(potager)
-n, m = potager.shape
-
-potager_voulue = np.array([
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2]
-])
-
-# On trouve le nombre minimum de points d'eau nécessaires
-# On mesure le temps d'exécution de l'algorithme
-nombre_parcelles_a_arroser = np.sum(potager_voulue > 1)
-start = time()
-points_d_eau, _ = trouver_points_d_eau(potager, n, m, potager_voulue, nombre_parcelles_a_arroser)
-end = time()
-print(end - start)
-
-if points_d_eau == float("inf"):
-    print("Aucune solution n'a été trouvée")
-else:
-    print("Points d'eau nécessaires :", points_d_eau)
+        # Mise en forme de la solution sous forme de matrice longueur*longueur
+        X_opt = X_opt.reshape(longueur, longueur)
+        return X_opt
+    else:
+        return None
